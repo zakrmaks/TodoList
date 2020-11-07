bab5d2803cbaff2b9f55c768dc9ca907a79966c3
zakrm <zakrmaks@gmail.com>
2020-11-07 23:10:43 +0200

    implemented returning method of lists of Category && Priority. implemented StatRepository.

--- TodoList/src/main/java/com/maks/todoList/controller/PriorityController.java
+++ TodoList/src/main/java/com/maks/todoList/controller/PriorityController.java
@@ -1,30 +1,82 @@
 package com.maks.todoList.controller;
 
+import com.maks.todoList.entity.Category;
 import com.maks.todoList.entity.Priority;
 import com.maks.todoList.repository.PriorityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/priority")
 public class PriorityController {
     private PriorityRepository priorityRepository;
 
+
     @Autowired
     public PriorityController(PriorityRepository priorityRepository) {
         this.priorityRepository = priorityRepository;
     }
 
-    @GetMapping("/test")
-    public List<Priority> test(){
-List<Priority> priorities = priorityRepository.findAll();
-return priorities;
+    //return list of priorities sorted by id
+    @GetMapping("/all")
+    public List<Priority> findAll() {
+        return priorityRepository.findAllByOrderByIdAsc();
+
+    }
+
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Priority> findById(@PathVariable Long id) {
+        Priority priority = null;
+        try {
+            priority = priorityRepository.findById(id).get();
+        }catch (NoSuchElementException e){
+            return new ResponseEntity("id="+id+" not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priority);
     }
 
     @PostMapping("/add")
-    public Priority add(@RequestBody Priority priority){
-      return  priorityRepository.save(priority);
+    public ResponseEntity<Priority> add(@RequestBody Priority priority){
+        // check getid because autoincrement of id in DB
+        if (priority.getId() != null && priority.getId() != 0){
+            return new ResponseEntity("id MUST be null", HttpStatus.ACCEPTED);
+        }
+        if (priority.getTitle() == null && priority.getTitle().trim().length() == 0){
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0){
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Priority priority) {
+        if (priority.getId() == null && priority.getId() == 0) {
+            return new ResponseEntity("id MUST be not null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getTitle() == null && priority.getTitle().trim().length() == 0){
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0){
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id) {
+
+        try {
+             priorityRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            return new ResponseEntity("id="+id+" not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
