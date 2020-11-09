4742b10c20076e6fb71d3746ccb0e7e60bb2c3c5
zakrm <zakrmaks@gmail.com>
2020-11-09 21:54:38 +0200

    bug removed

--- TodoList/src/main/java/com/maks/todoList/repository/TaskRepository.java
+++ TodoList/src/main/java/com/maks/todoList/repository/TaskRepository.java
@@ -1,16 +1,13 @@
 package com.maks.todoList.repository;
 
-import com.maks.todoList.entity.Priority;
-import com.maks.todoList.entity.Stat;
 import com.maks.todoList.entity.Task;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
@@ -21,7 +18,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
             "(:completed is null or c.completed=:completed) and " +
             "(:priorityId is null or c.priority.id=:priorityId) and " +
             "(:categoryId is null or c.category.id=:categoryId)")
-    Page<Task> findByParams(@Param("title") String title, @Param("completed") Integer completed, @Param("priorityId") Long priorityId, @Param("categoryId") Long categoryId, PageRequest pageRequest);
+    Page<Task> findByParams(@Param("title") String title, @Param("completed") Integer completed, @Param("priorityId") Long priorityId, @Param("categoryId") Long categoryId, Pageable pageable);
 
-    List<Task> findAllByOrderById();
 }
