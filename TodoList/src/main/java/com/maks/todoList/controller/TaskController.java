3b2ebb4258987f59f9abf78c76dc2ac7e0da14b7
zakrm <zakrmaks@gmail.com>
2020-11-10 09:50:37 +0200

    code review.

--- TodoList/src/main/java/com/maks/todoList/controller/TaskController.java
+++ TodoList/src/main/java/com/maks/todoList/controller/TaskController.java
@@ -6,6 +6,8 @@ import com.maks.todoList.search.TaskSearchValues;
 import com.maks.todoList.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -29,28 +31,29 @@ public class TaskController {
 
     @GetMapping("/id/{id}")
     public ResponseEntity<Task> findById(@PathVariable Long id) {
-        return taskService.findById(id);
+        return ResponseEntity.ok(taskService.findById(id));
     }
 
     @PostMapping("/add")
     public ResponseEntity<Task> add(@RequestBody Task task) {
 
-        return taskService.add(task);
+        return ResponseEntity.ok(taskService.add(task));
     }
 
     @PutMapping("/update")
     public ResponseEntity update(@RequestBody Task task) {
-        return  taskService.update(task);
+        return  ResponseEntity.ok(taskService.update(task));
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity deleteById(@PathVariable Long id) {
-        return taskService.deleteById(id);
+         taskService.deleteById(id);
+       return ResponseEntity.ok(HttpStatus.OK);
     }
 
     // search for different param taskSearchValues
     @PostMapping("/search")
     public ResponseEntity<Page<Task>> search(@RequestBody TaskSearchValues taskSearchValues) {
-      return   taskService.search(taskSearchValues);
+      return   ResponseEntity.ok(taskService.search(taskSearchValues));
     }
 }
